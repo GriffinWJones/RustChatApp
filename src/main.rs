3ef93d99e6565d8eaf4becd56c68d92a77d71d11
GriffinWJones <griffinwoodjones@gmail.com>
2025-03-10 18:34:55 -0700

    working tcp server that accepts 1 client and echos message

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,36 @@
-// use std::collections::VecDeque;
-// use std::sync::Arc;
-// use tokio::io::{AsyncReadExt, AsyncWriteExt};
-// use tokio::net::{TcpListener, TcpStream};
-// use tokio::sync::Mutex;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
 
-// #[tokio::main]
+#[tokio::main]
+async fn main() -> tokio::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+    println!("Server listening on port 8080...");
 
-// async fn main() -> tokio::io::Result<()> {
-//     let clients: Arc<Mutex<VecDeque<TcpStream>>> = Arc::new(Mutex::new(VecDeque::new()));
+    loop {
+        let (mut socket, addr) = listener.accept().await?;
+        println!("New client connected: {}", addr);
 
-//     let listener: TcpListener = TcpListener::bind("127.0.0.1:7878").await?;
-//     print!("Server listening on port 7878...");
+        tokio::spawn(async move {
+            let mut buffer = [0; 1024];
 
-//     loop {
-//         let (mut socket, addr) = listener.accept().await?;
-//         println!("New client connected: {}", addr);
-
-//         let clients = Arc::clone(&clients);
-//         tokio::spawn(async move {
-//             let mut socket = socket;
-//             let mut buffer = [0; 1024];
-
-//             {
-//                 let mut clients = clients.lock().await;
-//                 clients.push_back(socket);
-//             }
-//             loop {
-//                 match socket.read(&mut buffer).await {
-//                     Ok(0) => {
-//                         println!("Client {} disconnected", addr);
-//                         break;
-//                     }
-//                     Ok(n) => {
-//                         if let Err(e) = socket.write_all(&buffer[..n]).await {
-//                             eprintln!("Failed to write to client {}: {}", addr, e);
-//                             break;
-//                         }
-//                     }
-//                     Err(e) => {
-//                         eprintln!("Error reading from client {}: {}", addr, e);
-//                         break;
-//                     }
-//                 }
-//             }
-//             {
-//                 let mut clients = clients.lock().await;
-//                 clients.retain(|client| client.peer_addr().unwrap() != addr);
-//             }
-//         });
-//     }
-// }
+            loop {
+                match socket.read(&mut buffer).await {
+                    Ok(0) => {
+                        println!("Client {} disconnected", addr);
+                        break;
+                    }
+                    Ok(n) => {
+                        if let Err(e) = socket.write_all(&buffer[..n]).await {
+                            eprintln!("Failed to write to client {}: {}", addr, e);
+                            break;
+                        }
+                    }
+                    Err(e) => {
+                        eprintln!("Error reading from client {}: {}", addr, e);
+                        break;
+                    }
+                }
+            }
+        });
+    }
+}
