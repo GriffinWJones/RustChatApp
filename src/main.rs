9a77c6f092661e06f001752f2b7b35f0db3af144
GriffinWJones <griffinwoodjones@gmail.com>
2025-03-12 08:50:42 -0700

    added comments to each line

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt}; // Async read and write for network sockets
+use tokio::net::TcpListener; // Allows sever to listen for TCP connections
 
-#[tokio::main]
+#[tokio::main] // Initalizes tokio runtine, allows main to be async
 async fn main() -> tokio::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+    // Makes main async, returns a tokio::io::Result<()>
+    let listener = TcpListener::bind("127.0.0.1:8080").await?; // Binders a tcp listener to ip and port and waits until binding is complete
     println!("Server listening on port 8080...");
 
     loop {
-        let (mut socket, addr) = listener.accept().await?;
+        let (mut socket, addr) = listener.accept().await?; //waits for a client connect and gets the socket and the client's address
         println!("New client connected: {}", addr);
 
         tokio::spawn(async move {
+            // tokio::spawn Creates an async task (light weight thread) for client connection, move ensures ownwership of socket and addr in the task
+            // This means that main cannot use socket and addr as they are now owned by the task and not main
             let mut buffer = [0; 1024];
 
             loop {
